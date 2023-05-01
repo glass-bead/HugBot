4a2ae20b434732ab8e2b123f0701275c0ead2eea
Carolina Veloso <carolina.mg.veloso@gmail.com>
2023-05-01 18:34:40 +0100

    Add /hug please command

--- commands/hug.js
+++ commands/hug.js
@@ -8,8 +8,19 @@ module.exports = {
             subcommand
                 .setName('me')
                 .setDescription('Receive a warm hug from the bot.'), 
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('please')
+                .setDescription('Ask everyone for a hug.'), 
         ),
 	async execute(interaction) {
-		await interaction.reply(`A big warm hug to you, ${interaction.user} !!`);
+        var subcommand = interaction.options.getSubcommand();
+        if (subcommand == 'me' ) {
+            await interaction.reply(`A big warm hug to you, ${interaction.user} !!`);
+        }
+        else if ( subcommand == 'please') {
+            await interaction.reply(`@everyone emergency! ${interaction.user} is asking for a hug!!`);
+        }	
 	},
 };
